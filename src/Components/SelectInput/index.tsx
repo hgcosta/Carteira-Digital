ed15dde0932a0a5e0dc43a781a3b455e05fcc5d2
hgcosta <hugocosta.devs@gmail.com>
2023-05-03 20:55:57 -0300

    bugfix: update SelectInput component key index

--- src/Components/SelectInput/index.tsx
+++ src/Components/SelectInput/index.tsx
@@ -13,8 +13,10 @@ const SelectInput: React.FC<ISelectInputProps> = ({ options }) => {
   return (
     <Container>
       <select>
-        {options.map((option) => (
-          <option value="{option.value}">{option.label}</option>
+        {options.map((option, index) => (
+          <option key={index} value="{option.value}">
+            {option.label}
+          </option>
         ))}
       </select>
     </Container>
