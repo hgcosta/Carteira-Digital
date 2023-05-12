6d1cabcd2aef5b2356bf49026bc58609df11ec5b
hgcosta <hugocosta.devs@gmail.com>
2023-05-12 14:41:02 -0300

    feature: select input for year and month dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,31 +1,83 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 
 import { Container } from "./styles";
 
 import SelectInput from "../../Components/SelectInput";
 
 import ContentHeader from "../../Components/ContentHeader";
+import ListOfmonths from "../../utils/months";
+
+import expenses from "../../repositories/expenses";
+import gains from "../../repositories/gains";
 
 const Dashboard: React.FC = () => {
-  const options = [
-    {
-      value: "Rodrigo",
-      label: "Rodrigo",
-    },
-    {
-      value: "Hugo",
-      label: "Hugo",
-    },
-    {
-      value: "Hc",
-      label: "Hc",
-    },
-  ];
+  const [monthSelected, setMonthSelected] = useState<number>(
+    new Date().getMonth() + 1
+  );
+  const [yearSelected, setYearSelected] = useState<number>(
+    new Date().getFullYear()
+  );
+
+  const months = useMemo(() => {
+    return ListOfmonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month,
+      };
+    });
+  }, []);
+
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    [...expenses, ...gains].forEach((item) => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map((year) => {
+      return {
+        value: year,
+        label: year,
+      };
+    });
+  }, []);
+
+  const handleMonthSelected = (month: string) => {
+    try {
+      const parseMonth = Number(month);
+      setMonthSelected(parseMonth);
+    } catch (error) {
+      throw new Error("Invalid Month value. Is Accept 0 - 24");
+    }
+  };
+
+  const handleYearsSelected = (year: string) => {
+    try {
+      const parseYear = Number(year);
+      setYearSelected(parseYear);
+    } catch (error) {
+      throw new Error("Invalid Year value. Is accept inter number");
+    }
+  };
 
   return (
     <Container>
       <ContentHeader title="Dashboard" lineColor="#e44c4e">
-        <SelectInput options={options} onChange={() => {}} />
+        <SelectInput
+          options={months}
+          onChange={(e) => handleMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => handleYearsSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       </ContentHeader>
     </Container>
   );
