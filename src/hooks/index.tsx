78134ec86cefe5bd0d6ab5b33c88c37d7fa8d889
hgcosta <hugocosta.devs@gmail.com>
2023-05-28 17:50:25 -0300

    feature: saved theme select from localStorage

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -30,12 +30,22 @@ interface ITheme {
 const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
 const ThemeProvider: React.FC<ChildrenProps> = ({ children }) => {
-  const [theme, setTheme] = useState<ITheme>(dark);
+  const [theme, setTheme] = useState<ITheme>(() => {
+    const themeSaved = localStorage.getItem("@minha-carteira:theme");
+
+    if (themeSaved) {
+      return JSON.parse(themeSaved);
+    } else {
+      return dark;
+    }
+  });
   const toggleTheme = () => {
     if (theme.title === "dark") {
       setTheme(light);
+      localStorage.setItem("@minha-carteira:theme", JSON.stringify(light));
     } else {
       setTheme(dark);
+      localStorage.setItem("@minha-carteira:theme", JSON.stringify(dark));
     }
   };
 
