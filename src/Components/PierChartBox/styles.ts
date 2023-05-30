db2f94ca80f696e276e0dbef572f9f0801d3d1e2
hgcosta <hugocosta.devs@gmail.com>
2023-05-29 21:47:18 -0300

    feature: responsive pierchatbox component

--- src/Components/PierChartBox/styles.ts
+++ src/Components/PierChartBox/styles.ts
@@ -12,12 +12,32 @@ export const Container = styled.div`
   color: ${(props) => props.theme.colors.white};
   border-radius: 7px;
   display: flex;
+
+  @media (max-width: 770px) {
+    display: flex;
+    width: 100%;
+  }
 `;
 
 export const SideLeft = styled.aside`
   padding: 30px 20px;
   > h2 {
     margin-bottom: 20px;
+  }
+
+  @media (max-width: 1345) {
+    padding: 0 15px 5px;
+    margin-bottom: 7px;
+
+    > h2 {
+      margin-top: 15px;
+      margin-bottom: 7px;
+    }
+  }
+
+  @media (max-width: 420px) {
+    padding: 15px;
+    margin-bottom: 7px;
   }
 `;
 
@@ -38,6 +58,11 @@ export const LegendContainer = styled.ul`
   ::-webkit-scrollbar-track {
     background-color: ${(props) => props.theme.colors.tertiary};
   }
+
+  @media (max-width: 1345px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export const Legend = styled.li<ILegendProps>`
@@ -56,6 +81,19 @@ export const Legend = styled.li<ILegendProps>`
   > span {
     margin-left: 5px;
   }
+  @media (max-width: 1345px) {
+    font-size: 14px;
+    margin: 3px 0;
+
+    > div {
+      height: 35px;
+      width: 35px;
+      line-height: 35px;
+    }
+    > span {
+      margin-left: 7px;
+    }
+  }
 `;
 
 export const SideRight = styled.main`
@@ -64,4 +102,7 @@ export const SideRight = styled.main`
   align-items: center;
   height: 260px;
   width: 260px;
+  @media (max-width: 1345px) {
+    height: 100%;
+  }
 `;
