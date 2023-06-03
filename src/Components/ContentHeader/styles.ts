ba7d64d2d40dafdfad33471d89256d1bae42f1ef
hgcosta <hugocosta.devs@gmail.com>
2023-06-02 22:18:37 -0300

    fix: update components

--- src/Components/ContentHeader/styles.ts
+++ src/Components/ContentHeader/styles.ts
@@ -9,8 +9,11 @@ export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
   margin-bottom: 25px;
+
+  @media (max-width: 320px) {
+    flex-direction: column;
+  }
 `;
 
 export const TitleContainer = styled.div<ITitleContainer>`
@@ -24,8 +27,18 @@ export const TitleContainer = styled.div<ITitleContainer>`
       border-bottom: solid 4px ${(props) => props.lineColor};
     }
   }
+  @media (max-width: 420px) {
+    > h1 {
+      font-size: 22px;
+    }
+  }
 `;
 
 export const Controller = styled.div`
   display: flex;
+  @media (max-width: 320px) {
+    width: 100%;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
 `;
